526b097459ea1640d0083fd130351f6d47a749cb
Tom Odon <todon@perkspot.com>
2018-01-18 19:00:19 -0600

    using correct array length to compare first 8 armstrong numbers

--- js/armstrongNumbersSpec.js
+++ js/armstrongNumbersSpec.js
@@ -6,6 +6,6 @@ function createArrayOfNum(maxValue) {
 }
 
 console.log(shallowEqualArrays(arm.findArmstrongNumbers([0]),[0]);
-console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(7)), [0, 1, 2, 3, 4, 5, 6, 7]);
+console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(8)), [0, 1, 2, 3, 4, 5, 6, 7]);
 console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(99)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 console.log(shallowEqualArrays(arm.findArmstrongNumbers(createArrayOfNum(999)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]);
