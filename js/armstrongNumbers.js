bd9e50a197912634bee893c0c5f8ef573a60a65c
stesun2 <ss27@illinois.edu>
2021-05-27 14:40:53 -0500

    js rough draft, need to complete test cases

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,21 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(num) {
+    let numArr = Array.from(num.toString());
+    //create value for arr length
+    console.log(numArr)
+    let numArrLength = numArr.length;
+    let result = 0;
+    for (let i = 0; i < numArrLength; i++) {
+      if (numArrLength > 1) {
+        result += numArr[i] ** numArrLength;
+      } else {
+        result += numArr[i] ** 1;
+      }
+    }
+    return result;
+     //iterate through numArr
+    //if arr length is greater than 1 power that by length else power that by 1
+  };
 
-};
+console.log(exports.findArmstrongNumbers(12))
