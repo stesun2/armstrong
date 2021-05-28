5c1995c9a94286e0d20b8ebd0b2f1e54d88cf350
stesun2 <ss27@illinois.edu>
2021-05-28 10:34:26 -0500

    Toms solution worked along

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,3 +1,33 @@
 # How can you make this more scalable and reusable later?
 # Testing
+
+# ------ PSEUDOCODE ------
+# Write function/method that takes in an argument that is a RANGE OF NUMBERS
+# Create an ANSWER var that is an array
+# ITERATE/LOOP through the range of number
+# Convert each num into a string
+# Create var called EXPONENT == length of string number
+# Create var called SUM and have its initial value = 0
+# Create a TEMP var that stores a number to extract each individual integer from the number
+# Create a WHILE LOOP where TEMP is GREATER THAN 0
+# Find the remainder of the TEMP by using MOD 10 to last int and store it into a var called DIGIT
+# Take the DIGIT var and take the power of the EXPONENT variable and ADD to the SUM
+# reassign the TEMP var to the floor of the TEMP / 10
+# Write an IF STATEMENT that checks if NUM is equal to SUM and if it's true, add it to the ANSWER var
+
+# Tom's Solution Work along
 def find_armstrong_numbers(numbers):
+    answer = []
+    for num in numbers:
+        exponent = len(str(num))
+        sum = 0
+        temp = num
+        while temp > 0:
+            digit = temp % 10
+            sum += digit ** exponent
+            temp //= 10  # Taking the floor
+        if num == sum:
+            answer.append(num)
+    return answer
+
+print(find_armstrong_numbers(list(range(0,999))))
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,7 +1,35 @@
 # The code is written as driver code. Can you convert it to Unit Test?
+
+import unittest
 from armstrong_numbers import find_armstrong_numbers
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+class ArmstrongNumbersTestCase(unittest.TestCase):
+
+    def test_returns_a_list(self):
+        """When you call find_armstrong_numbers, it should return an array/list."""
+        self.assertEqual(type(find_armstrong_numbers(list(range(0,8)))), list)
+
+    def test_returns_the_correct_list(self):
+        """When you call: find_armstrong_numbers(list)"""
+        self.assertEqual(type(find_armstrong_numbers(list(range(0,8)))), [0, 1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(type(find_armstrong_numbers(list(range(0,100)))), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(type(find_armstrong_numbers(list(range(0,999)))), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# print(find_armstrong_numbers([0]) == [0]) 
+# print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
+# print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
